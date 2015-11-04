b890786b2439430d705f4d9fd8beb639c0316640
Doveccl <i@doveccl.com>
2015-11-04 12:00:21 +0800

    Add "install apache php and mysql"

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get -y --force-yes install python g++ fpc
 sudo apt-get -y --force-yes install python-dev python-mysqldb
+sudo apt-get -y --force-yes install apache2 php5 mysql-server php5-mysql
 sudo apt-get -y --force-yes install git
 
 echo "Installing doj core ..."
