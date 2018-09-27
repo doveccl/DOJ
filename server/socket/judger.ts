b9b2666602a7b9258af17e6e0b9e1e3ec5b2f79c
Doveccl <doveccl@live.com>
2018-09-27 15:44:54 +0800

    fix: layout style & judging queue

--- server/socket/judger.ts
+++ server/socket/judger.ts
@@ -1,6 +1,7 @@
 import * as config from 'config'
 import * as IO from 'socket.io'
 
+import { compare } from '../../common/function'
 import { Pack, SE } from '../../common/pack'
 import { problem } from '../middleware/fetch'
 import { DSubmission } from '../model/submission'
@@ -12,7 +13,7 @@ let currentNS: IO.Namespace
 const secret: string = config.get('secret')
 
 const judgers: string[] = []
-const judgings: { [index: string]: string[] } = {}
+const judgings: { [index: string]: any[] } = {}
 
 const addJudger = (id: string, concurrent: number) => {
 	let count = concurrent
@@ -43,7 +44,8 @@ export const routeJudger = (io: IO.Namespace, socket: IO.Socket) => {
 		if (!pack || !pack._id) { return }
 		addJudger(socket.id, 1)
 		for (let i = judgings[socket.id].length - 1; i >= 0; i--) {
-			if (judgings[socket.id][i] === pack._id) {
+			if (compare(judgings[socket.id][i]._id, pack._id)) {
+				logSocket.debug('Update submission:', pack)
 				judgings[socket.id].splice(i, 1)
 			}
 		}
@@ -52,8 +54,9 @@ export const routeJudger = (io: IO.Namespace, socket: IO.Socket) => {
 	socket.on('disconnect', () => {
 		if (judgers.includes(socket.id)) {
 			logSocket.info('Del judger:', socket.id)
-			for (const sid of judgings[socket.id]) {
-				update(SE(sid, 'judger disconnected'))
+			for (const submission of judgings[socket.id]) {
+				logSocket.debug('Update judging queue:', submission._id)
+				update(SE(submission._id, 'judger disconnected'))
 			}
 			delete judgings[socket.id]
 			delJudger(socket.id)
