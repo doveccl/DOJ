36c4077a975be14ad71a19f5bc512361822f8c52
Doveccl <doveccl@live.com>
2020-11-14 08:30:29 +0800

    update

--- judger/judge.ts
+++ judger/judge.ts
@@ -24,7 +24,6 @@ export async function judge(args: IJudge) {
 	const language = languages[args.language]
 	const dataPath = await prepareData(args.data)
 	const runPath = `${runRoot}/${args._id}`
-	await fs.chmod(runPath, 0o777)
 
 	await fs.outputFile(`${runPath}/${language.source}`, args.code)
 	fs.readdirSync(dataPath).forEach(file => {
@@ -36,6 +35,7 @@ export async function judge(args: IJudge) {
 
 	// compile
 	if (language.compile) {
+		await fs.chmod(runPath, 0o777)
 		const result = lrunSync({
 			cmd: language.compile.cmd,
 			args: language.compile.args,
