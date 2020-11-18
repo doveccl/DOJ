7a9076439ce32f72206ecca4cbc6968f822931ba
Doveccl <doveccl@live.com>
2020-11-19 01:49:10 +0800

    update copyright

--- web/layout/footer/index.tsx
+++ web/layout/footer/index.tsx
@@ -7,8 +7,11 @@ import './index.less'
 export default class extends React.Component {
 	public render() {
 		return <Layout.Footer className="footer">
-			<span>Copyright &copy; 2018 </span>
+			<span>&copy; 2015 - {(new Date).getFullYear()}</span>
+			<span>&nbsp;&nbsp;</span>
 			<a target="_blank" href="https://github.com/doveccl/DOJ">Doveccl Online Judge</a>
+			<span>&nbsp;&nbsp;</span>
+			<span>All Rights Reserved</span>
 		</Layout.Footer>
 	}
 }
