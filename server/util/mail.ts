2f8ff4b16c2679c74b5699ad956acea779c67cf7
Doveccl <doveccl@live.com>
2021-09-05 02:07:28 +0800

    update

--- server/util/mail.ts
+++ server/util/mail.ts
@@ -1,8 +1,8 @@
-import { get } from 'config'
+import config from 'config'
 import { createTransport } from 'nodemailer'
 
-const transporter = createTransport(get('mail'))
-const from = `"DOJ System" <${get('mail.auth.user')}>`
+const	transporter = createTransport(config.get('mail'))
+const	from = `"DOJ System" <${config.get('mail.auth.user')}>`
 
 export function send(to: string, subject: string, text: string) {
 	return transporter.sendMail({ from, to, subject, text })
