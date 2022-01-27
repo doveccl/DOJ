886a374be99eadba653b28fee5a7fc22757445e0
Doveccl <doveccl@live.com>
2022-01-27 17:43:52 +0800

    init commit

--- server/util/mail.ts
+++
@@ -1,9 +0,0 @@
-import { createTransport } from 'nodemailer'
-import { config } from './config'
-
-const	transporter = createTransport(config.mail)
-const	from = `"DOJ System" <${config.mail.auth.user}>`
-
-export function send(to: string, subject: string, text: string) {
-	return transporter.sendMail({ from, to, subject, text })
-}
--- web/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './header'
-export { default as Sider } from './sider'
-export { default as Footer } from './footer'
